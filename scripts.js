76dcb4403122049b495015258dace8a3e30b26f2
michaelstaerke <michael.staerke@mail.de>
2021-05-15 00:15:46 +0200

    Update: Content changes mostly in html

--- scripts.js
+++ scripts.js
@@ -3,6 +3,9 @@
 @author:     Mike Stk
 
 JS Script to test if the guest data is valid.
+
+main source: https://www.codebrainer.com/blog/contact-form-in-javascript 
+             https://www.tutorialrepublic.com/javascript-tutorial/javascript-form-validation.php
 */
 
 
@@ -18,12 +21,27 @@ function myFunction() {
 
 
 
-/* Script: Validation */
+/* Script: Contact Form Validation */
+
+// guest Data: guestName guestEmail guestTopic guestMessage guestPrivacyPolicy guestSendButton
+
 
 function validateForm() {
     
-    
-    /* Function description */
+
+    // var which groups all fields into an object
+    var fields = {};
+
+    document.addEventListener("DOMContentLoaded", function() {
+
+        fields.guestName = document.getElementById('guestName');
+        fields.guestEmail = document.getElementById('guestEmail');
+        fields.guestTopic = document.getElementById('guestEmail');
+        fields.guestMessage = document.getElementById('guestEmail');
+    })
+
+
+    /* Function that checks, if the guest-field is not empty */
     function isNotEmpty(value) {
 
         if (value == null || typeof value == 'undefined')
@@ -33,20 +51,22 @@ function validateForm() {
     }
 
 
-    /* Function description */
+    /* Function that checks, if an number guest-field is filled with numbers */
     function isNumber(num) {
 
         return (num.length > 0) && !isNan(num);
     }
 
-    /* Function description */
+
+    /* Function that checks, if the guest-field is an email */
     function isEmail(email) {
         let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9]{0,61}[a-ZA-Z0-9])?)*$/;
         return regex.test(String(email).toLowerCase());
     }
 
 
-    /* Function description */
+    /* Function that checks, if all the validation functions are in order,
+        to use them with the main validation function */
     function fieldValidation(field, validationFunction) {
 
         if (field == null) 
@@ -73,31 +93,34 @@ function validateForm() {
         valid &= fieldValidation(fields.guestEmail, isEmail);
         valid &= fieldValidation(fields.guestTopic, isNotEmpty);
         valid &= fieldValidation(fields.guestMessage, isNotEmpty);
-        valid &= fieldValidation(fields.guestName, isNotEmpty);
-
     }
     
     
-    
-    
-}    
+    /* Class which combines multiple values in one, makes it easier to work with complex values */
+    class User {
 
+        constructor(guestName, guestEmail, guestTopic, guestMessage){
 
-
-/*
-function sendGuestContact() {
-    if () {
-        document.getElementById("guestSendButton").innerHTML = 'Test'; 
+            this.guestName = guestName;
+            this.guestEmail = guestEmail;
+            this.guestTopic = guestTopic;
+            this.guestMessage = guestMessage;
+        }
     }
-    else {
-        window.alert();
+
+
+    /* main function, which is called from the HTML button */
+    function sendContant() {
+    
+        if (isValid()) {
+            let usr = new User(guestName.value, guestEmail.value, guestTopic.value, guestMessage.value);
+
+            alert('${usr.guestName} Danke für die Nachricht.')
+        
+        } else {
+
+            alert("Da war ein Fehler!")
+        }
     }
+
 }
-*/
-
-/* document.getElementById("demo").innerHTML */
-
-/* 
-Source 0: https://www.codebrainer.com/blog/contact-form-in-javascript 
-Source 1: https://www.tutorialrepublic.com/javascript-tutorial/javascript-form-validation.php
-*/
